c4dc3e6615a7ba7654b2147ee5a841c09b5c9685
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-11-13 15:16:23 +0100

    json on the card

--- Assets/#Project/Scripts/CardBehavior.cs
+++ Assets/#Project/Scripts/CardBehavior.cs
@@ -11,10 +11,10 @@ public class CardBehavior : MonoBehaviour
     [SerializeField] private Canvas canvas;
 
     // The card knows its contents : a question, the corresponding image and both possible answers.
-    [SerializeField] private Image background;
+    // [SerializeField] private Image background;
     [SerializeField] private Image image;
 
-    [SerializeField] private TextMeshProUGUI question;
+    [SerializeField] private TextMeshProUGUI titre;
 
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TextMeshProUGUI accept;
@@ -28,9 +28,9 @@ public class CardBehavior : MonoBehaviour
     public void Initialize()
     {
         canvas.gameObject.SetActive(false);
-        background.gameObject.SetActive(true);
+        // background.gameObject.SetActive(true);
         image.gameObject.SetActive(true);
-        question.gameObject.SetActive(true);
+        titre.gameObject.SetActive(true);
         text.gameObject.SetActive(true);
         accept.gameObject.SetActive(true);
         reject.gameObject.SetActive(true);
@@ -41,18 +41,18 @@ public class CardBehavior : MonoBehaviour
     {
         // illustration.sprite = illustrationSprite;
 
-        question.text = questionText;
+        titre.text = questionText;
         text.text = textText;
         accept.text = response1Text;
         reject.text = response2Text;
 
-        background.gameObject.SetActive(true);
+        // background.gameObject.SetActive(true);
         canvas.gameObject.SetActive(true);
     }
 
     public void HideQuestion()
     {
-        background.gameObject.SetActive(false);
+        // background.gameObject.SetActive(false);
         canvas.gameObject.SetActive(false);
     }
 }
--- Assets/#Project/Scripts/GameManager.cs
+++ Assets/#Project/Scripts/GameManager.cs
@@ -33,13 +33,9 @@ public class GameManager : MonoBehaviour
     // json data
     [SerializeField] private TextAsset jsonFile;
 
-    // score
-    [SerializeField] private int scoreMax = 20;
-    [SerializeField] private int scoreMin = 0;
-
     private CardList cardList;
     private int currentCardIndex = 0;
-    private int totalCards = 0;
+    private int totalCards = 0; // à definir
 
     private int totalScore = 0;
 
@@ -59,7 +55,7 @@ public class GameManager : MonoBehaviour
     private void SetupProgressBar() // instantie progress bar
     {
         progressBar.minValue = 0;
-        progressBar.maxValue = 20; 
+        progressBar.maxValue = totalCards; // à definir
         progressBar.value = 0;
     }
 
@@ -67,7 +63,7 @@ public class GameManager : MonoBehaviour
     {
         if (currentCardIndex >= totalCards) // si toutes les cartes sont passé --> fin du jeu
         {
-            sceneLoader.ChangeScene("End3");
+            EndGame();
             return;
         }
 
@@ -95,35 +91,24 @@ public class GameManager : MonoBehaviour
         currentCardIndex++; // on fait +1 dans l'index
         progressBar.value = currentCardIndex; // met à jour la bar de progression
 
-         if (totalScore >= scoreMax)
-        {
-            sceneLoader.ChangeScene("End2"); // jauge max atteinte
-            return;
-        }
-        else if (totalScore <= scoreMin)
-        {
-            sceneLoader.ChangeScene("End"); // jauge min atteinte
-            return;
-        }
-
         CreateNextCard(); // crée la carte d'apres
     }
     
-    // private void EndGame() 
-    // {
-    //     if (totalScore <= 9)
-    //     {
-    //         sceneLoader.ChangeScene("End");
-    //     }
-    //     else if (totalScore <=20)
-    //     {
-    //         sceneLoader.ChangeScene("End2");
-    //     }
-    //     else
-    //     {
-    //         sceneLoader.ChangeScene("End3");
-    //     }
-    // }
+    private void EndGame() // toutes les cartes ont été jouées, lance la scene End
+    {
+        if (totalCards <= 9)
+        {
+            sceneLoader.ChangeScene("End");
+        }
+        else if (totalScore <=20)
+        {
+            sceneLoader.ChangeScene("End2");
+        }
+        else
+        {
+            sceneLoader.ChangeScene("End3");
+        }
+    }
 
     public void GameStarted() // lancement de la main scene
     {
