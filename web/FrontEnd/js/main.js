3ce046647bb0a10fb29cc21e2b32b1b534a9c83c
Audrey Sonkes <a.sonkes@interface3.be>
2025-11-14 15:06:04 +0100

    js en ordre

--- web/FrontEnd/js/main.js
+++ web/FrontEnd/js/main.js
@@ -3,26 +3,28 @@ const menuDiv = document.getElementById("menu");
 const startBtn = document.getElementById("start-btn");
 
 // when clicked, closes the menu and opens the GamePlay
-startBtn.addEventListener("click", () => {
-  menuDiv.style.display = "none";
-  gamePlayContainerDiv.style.display = "block";
-  createUnityInstance(document.querySelector("#unity-canvas"), {
-    arguments: [],
-    dataUrl: "/Build/www_cockpit.data",
-    frameworkUrl: "Build/www_cockpit.framework.js",
-    codeUrl: "Build/www_cockpit.wasm",
-    streamingAssetsUrl: "StreamingAssets",
-    companyName: "DefaultCompany",
-    productName: "WebTutorial1",
-    productVersion: "0.1",
-    // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
-    // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
-  })
-    .then((unityInstance) => {})
-    .catch((message) => {
-      alert(message);
-    });
-});
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    menuDiv.style.display = "none";
+    gamePlayContainerDiv.style.display = "block";
+    createUnityInstance(document.querySelector("#unity-canvas"), {
+      arguments: [],
+      dataUrl: "/Build/www_cockpit.data",
+      frameworkUrl: "Build/www_cockpit.framework.js",
+      codeUrl: "Build/www_cockpit.wasm",
+      streamingAssetsUrl: "StreamingAssets",
+      companyName: "DefaultCompany",
+      productName: "WebTutorial1",
+      productVersion: "0.1",
+      // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+      // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+    })
+      .then((unityInstance) => {})
+      .catch((message) => {
+        alert(message);
+      });
+  });
+}
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
   // Mobile device style: fill the whole browser client area with the game canvas:
