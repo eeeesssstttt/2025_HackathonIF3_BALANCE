a164168ce02256a27d069632e04d315b03fa9f8a
Liene <l.bare@interface3.be>
2025-11-14 11:44:10 +0100

    updated img

--- Assets/#Project/Scripts/CardBehavior.cs
+++ Assets/#Project/Scripts/CardBehavior.cs
@@ -11,10 +11,10 @@ public class CardBehavior : MonoBehaviour
     [SerializeField] private Canvas canvas;
 
     // The card knows its contents : a question, the corresponding image and both possible answers.
-    // [SerializeField] private Image background;
+    [SerializeField] private Image background;
     [SerializeField] private Image image;
 
-    [SerializeField] private TextMeshProUGUI titre;
+    [SerializeField] private TextMeshProUGUI question;
 
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private TextMeshProUGUI accept;
@@ -28,31 +28,33 @@ public class CardBehavior : MonoBehaviour
     public void Initialize()
     {
         canvas.gameObject.SetActive(false);
-        // background.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
         image.gameObject.SetActive(true);
-        titre.gameObject.SetActive(true);
+        question.gameObject.SetActive(true);
         text.gameObject.SetActive(true);
         accept.gameObject.SetActive(true);
         reject.gameObject.SetActive(true);
     }
 
     // public void DisplayQuestion(Sprite illustrationSprite, string questionText, string response1Text, string response2Text)
-    public void DisplayQuestion(string questionText, string textText, string response1Text, string response2Text)
+    //string questionText, Sprite sprite, string textText, string response1Text, string response2Text
+    public void DisplayQuestion(string questionText, Sprite sprite, string textText)
+
     {
         // illustration.sprite = illustrationSprite;
 
-        titre.text = questionText;
+        question.text = questionText;
         text.text = textText;
-        accept.text = response1Text;
-        reject.text = response2Text;
-
-        // background.gameObject.SetActive(true);
+        image.sprite = sprite;
+        background.gameObject.SetActive(true);
         canvas.gameObject.SetActive(true);
     }
 
     public void HideQuestion()
     {
-        // background.gameObject.SetActive(false);
+        background.gameObject.SetActive(false);
         canvas.gameObject.SetActive(false);
     }
+
+    
 }
